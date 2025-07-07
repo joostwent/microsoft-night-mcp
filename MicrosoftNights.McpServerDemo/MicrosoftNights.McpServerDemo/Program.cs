969925e8767104889f956e533724239c135e68cc
Joost Went <joost.went@capgemini.com>
2025-07-07 16:11:28 +0200

    Hello World MCP Server

---
+++ MicrosoftNights.McpServerDemo/MicrosoftNights.McpServerDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MicrosoftNights.McpServerDemo.Tools;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<HelloWorldTool>();
+
+await builder.Build().RunAsync();
