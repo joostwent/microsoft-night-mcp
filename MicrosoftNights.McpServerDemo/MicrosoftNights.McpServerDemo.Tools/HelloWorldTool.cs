2e94740aa4eda9ca541f0c0b291c93ba6861c054
Joost Went <joost.went@capgemini.com>
2025-07-09 16:02:10 +0200

    CleanUp

--- MicrosoftNights.McpServerDemo/MicrosoftNights.McpServerDemo.Tools/HelloWorldTool.cs
+++ MicrosoftNights.McpServerDemo/MicrosoftNights.McpServerDemo.Tools/HelloWorldTool.cs
@@ -11,18 +11,6 @@ namespace MicrosoftNights.McpServerDemo.Tools
     [McpServerToolType]
     public class HelloWorldTool
     {
-        [McpServerTool(Name = "HelloNight"), Description("Returns Hello Microsoft Night to all the people in the room of the Microsoft Night")]
-        public static string HelloMicrosoftNight()
-        {
-            return "Hello wonderful audience of the Microsoft Night";
-        }
-
-        [McpServerTool(Name = "HelloPizza"), Description("Returns Bon Apetit to all the people eating pizzas")]
-        public static string HelloPizzaSession()
-        {
-            return "Hope you have a wonderful pizza. Bon appetit!";
-        }
-
         [McpServerTool(Name = "OrderStatus"), Description("Returns the order status if the user sends the order id, if the order is unknown, then offer the user the option to create a new order")]
         public static string GetOrderStatus([Description("The order id that was received by e-mail when the user submitted his order")] int orderId )
         {
