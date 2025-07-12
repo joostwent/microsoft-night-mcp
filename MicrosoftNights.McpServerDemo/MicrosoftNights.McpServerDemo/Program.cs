ca93b897b224cc7606706c909056c7e1bf68d732
Joost Went <joost.went@capgemini.com>
2025-07-12 11:53:53 +0200

    Removed log comments

--- MicrosoftNights.McpServerDemo/MicrosoftNights.McpServerDemo/Program.cs
+++ MicrosoftNights.McpServerDemo/MicrosoftNights.McpServerDemo/Program.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MicrosoftNights.McpServerDemo.Tools;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.AddConsole(consoleLogOptions =>
-{
-    // Configure all logs to go to stderr
-    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
-});
-
 builder.Services
     .AddMcpServer()
     .WithStdioServerTransport()
